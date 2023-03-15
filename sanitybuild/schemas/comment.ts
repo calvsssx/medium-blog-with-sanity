63dc13b9fb78c67d1b544b5bee34e07f5ccfa22b
calvsssx <calvin.quiambao@gmail.com>
2023-03-15 22:34:32 +0800

    add comment for blog post with api

---
+++ sanitybuild/schemas/comment.ts
@@ -0,0 +1,31 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'comment',
+  title: 'Comment',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      type: 'string',
+    }),
+    defineField({
+      name: 'approved',
+      title: 'Approved',
+      type: 'boolean',
+    }),
+    defineField({
+      name: 'email',
+      type: 'string',
+    }),
+    defineField({
+      name: 'comment',
+      type: 'text',
+    }),
+    defineField({
+      name: 'post',
+      type: 'reference',
+      to: [{type: 'post'}],
+    }),
+  ],
+})
